507dad93554893804e2eccfcd4d490d3d1533b76
CoveyAfolami <coveyafolami@gmail.com>
2025-07-18 10:02:13 +0100

    second build, bugs fixed

--- pidgeonGame_1/Assets/Scripts/PlayerMovement.cs
+++ pidgeonGame_1/Assets/Scripts/PlayerMovement.cs
@@ -8,6 +8,10 @@ public class PlayerMovement : MonoBehaviour
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
 
+    public GameObject poopProjectile;
+    private bool canPoop = true;
+    private bool hasPooped = false;
+
     // Glide settings
     public float maxGlideTime = 2f;  // Total allowed glide time
     private float glideTimeLeft = 2f;
@@ -21,9 +25,15 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
+
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
 
         // Glide: if not grounded and holding E
@@ -41,11 +51,12 @@ public class PlayerMovement : MonoBehaviour
             glideTimeLeft = maxGlideTime;
         }
 
-        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        if (IsGrounded())
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+            hasPooped = false;
         }
 
+        Poop();
         Flip();
     }
 
@@ -75,6 +86,16 @@ public class PlayerMovement : MonoBehaviour
         if (collision.CompareTag("Bedrock"))
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private void Poop()
+    {
+        if (!IsGrounded() && (canPoop && !hasPooped) && Input.GetKeyDown(KeyCode.W))
+        {
+            Instantiate(poopProjectile, transform.position, Quaternion.identity);
+            hasPooped = true;
+            
         }
     }
 }
